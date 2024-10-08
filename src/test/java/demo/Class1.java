e5c6f6514edeb0c4c584600dbfd7e3024ee3f1de
thrinadhram <110017774+thrinadhram@users.noreply.github.com>
2024-10-08 13:21:22 +0530

    Add files via upload
    commit in frst

---
+++ src/test/java/demo/Class1.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(" addition of a and b value are:"+c);
+		
+	}
+
+}
